4f475655cc3e910c39b886c357f0de20d75f373e
dharshanb0025 <dharshanb0025@gmail.com>
2025-08-15 23:23:49 +0530

    initial commit

---
+++ mediverify-backend/mediverify-backend/src/main/java/com/mediverify/mediverify_backend/MediVerifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediverify.mediverify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediVerifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediVerifyBackendApplication.class, args);
+	}
+
+}
---
+++ mediverify-backend/mediverify-backend/src/test/java/com/mediverify/mediverify_backend/MediVerifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediverify.mediverify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediVerifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
